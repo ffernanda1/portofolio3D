ea20ff29f4306c993d0087d64ad20569404c8709
Fernanda <mangorifernanda@gmail.com>
2023-07-19 17:15:18 +0700

    3DModels

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,14 +15,35 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
-          <h1 className={`${styles.heroHeadText} text-[#a2dea5]`}>Hi, I'm
+          <h1 className={`${styles.heroHeadText} text-[#99e0ac]`}>Hi, I'm
             <span className="text-[#52d959]"> Fernanda</span> </h1>
-          <p className={`${styles.heroSubText} mt-2 text-[#0a7310]`}>
+          <p className={`${styles.heroSubText} mt-2 text-[#99e0ac]`}>
             I develop Frontend and Backend <br className="sm:block hidden" /> for web applications React
           </p>
         </div>
       </div>
-      <ComputersCanvas/>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10
+        bottom-32 w-full flex justify-center
+        items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px]
+            rounded-3xl border-4 border-secondary
+            flex justify-center items-start p-2">
+            <motion.dev
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop"
+              }}
+              className="w-3 h-3 rounded-full
+              bg-secondary mb-1"/>
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,39 +4,53 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
-  
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+const Computers = ({isMobile}) => {
+
+  const computer = useGLTF('./bmoz/scene.gltf')
   return (
     <mesh>
-      <hemisphereLight intensity={0.15}
+      <hemisphereLight intensity={0.75}
         groundColor="black" />
       <pointLight intensity={1} />
-      <spotLight 
-      position={[-20, 50, 10]}
-      angle={0.12}
-      penumbra={1}
-      intensity={1}
-      castShadow
-      shadow-mapSize={1024}
+      <spotLight
+        position={[50, 40, 50]}
+        angle={10}
+        penumbra={1}
+        intensity={0.75}
+        castShadow
+        shadow-mapSize={1024}
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 20 : 30}
+        position={isMobile ? [0, -3.3, -2.2] : [0, -1.25, -1.5]}
+        rotation={[0, 0, 0]}
       />
     </mesh>
   )
 }
 
 const ComputersCanvas = () => {
-  // const set = useThree(state => state.set)
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width:693px)');
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange)
+    }
+  }, [])
+
+
   return (
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, 4, 20], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -44,7 +58,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile}/>
       </Suspense>
       <Preload all />
     </Canvas>
