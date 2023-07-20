3934d6d82acce03232d695758f41e2b4a03d12c8
Fernanda <mangorifernanda@gmail.com>
2023-07-20 22:59:18 +0700

    about done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const ServiceCard = ({ index, title, icon }) => (
   <Tilt className="xs:w-[250px] w-full">
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className="w-full grenn-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
     >
       <div
         options={{
@@ -19,14 +19,14 @@ const ServiceCard = ({ index, title, icon }) => (
           scale: 1,
           speed: 450
         }}
-        className="bg-tertiary rounded-[20px] 
+        className="bg-[#c9c9c9] rounded-[20px] 
           py-5 px-12 min-h-[280px] flex 
           justify-evenly items-center flex-col"
       >
         <img src={icon} alt='web-development'
-          className="w-16 h-16 object-contain"
+          className="w-50 h-50 object-contain"
         />
-        <h3 className="text-white text-[20px] font-bold text-center">
+        <h3 className="text-black text-[20px] font-bold text-center">
           {title}
         </h3>
       </div>
